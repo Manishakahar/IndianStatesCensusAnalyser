3cd687b1845aecf26361b259580f79d587a23d70
Manishakahar <manisha.kahar1998@gmail.com>
2022-01-02 23:46:08 +0530

    Added: Welcome Message

---
+++ src/main/java/com/blz/censusanalyser/CensusAnalyser.java
@@ -0,0 +1,7 @@
+package com.blz.censusanalyser;
+
+public class CensusAnalyser {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Indian States Census Analyser");
+    }
+}
