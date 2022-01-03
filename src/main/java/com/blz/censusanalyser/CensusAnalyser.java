99203d16927ed4237c219607e0d90e4e513d42c4
Manishakahar <manisha.kahar1998@gmail.com>
2022-01-04 00:24:57 +0530

    Refactor:Checked the Test Case StateCensusAnalyser to ensure Number of record.

--- src/main/java/com/blz/censusanalyser/CensusAnalyser.java
+++ src/main/java/com/blz/censusanalyser/CensusAnalyser.java
@@ -1,36 +1,25 @@
 package com.blz.censusanalyser;
 
-import com.opencsv.bean.CsvToBean;
-import com.opencsv.bean.CsvToBeanBuilder;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;;
 import java.nio.file.Paths;
 import java.util.Iterator;
+import java.util.stream.StreamSupport;
 
 public class CensusAnalyser {
 
-
     public int loadIndiaCensusData(String csvPath) throws CensusAnalyserException {
-
-        try {
-            Reader reader = Files.newBufferedReader(Paths.get(csvPath));
-            CsvToBeanBuilder<CSVStateCensus> CsvToBeanBuilder = new CsvToBeanBuilder<CSVStateCensus>(reader);
-            CsvToBeanBuilder.withType(CSVStateCensus.class);
-            CsvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-            CsvToBean<CSVStateCensus> csvToBean = CsvToBeanBuilder.build();
-            Iterator<CSVStateCensus> censusCSVIterator = csvToBean.iterator();
-            int numOfEntries = 0;
-            while (censusCSVIterator.hasNext()) {
-                numOfEntries++;
-                System.out.println(censusCSVIterator);
-                censusCSVIterator.next();
-            }
-            return numOfEntries;
+        try (Reader reader = Files.newBufferedReader(Paths.get(csvPath))) {
+            Iterator<CSVStateCensus> censusCSVIterator = new OpenCSV().getCSVIterator(reader, CSVStateCensus.class);
+            return    getCount(censusCSVIterator);
         } catch (IOException e) {
-            e.printStackTrace();
             throw new CensusAnalyserException(e.getMessage(),CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
         }
-
+    }
+    private <E> int getCount(Iterator<E> censusCSVIterator) {
+        Iterable<E> csvIterable = () -> censusCSVIterator;
+        int numberOfEntries = (int) StreamSupport.stream(csvIterable.spliterator(), true).count();
+        return numberOfEntries;
     }
 }
---
+++ src/main/java/com/blz/censusanalyser/OpenCSV.java
@@ -0,0 +1,17 @@
+package com.blz.censusanalyser;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.Reader;
+import java.util.Iterator;
+
+public class OpenCSV {
+    public <E> Iterator getCSVIterator(Reader reader, Class csvClass) {
+        CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder(reader);
+        csvToBeanBuilder.withType(csvClass);
+        csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+        CsvToBean<E> csvToBean = csvToBeanBuilder.build();
+        return csvToBean.iterator();
+    }
+}
